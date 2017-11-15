f015363956368b9f800eabc267709f5fd09b13d8
Naoki Kinoshita <uttnaoki@gmail.com>
2017-11-16 05:07:05 +0900

    Fix controllers_bag create

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -4,8 +4,9 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:paspassword])
-      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
